f9f84e85176196512be4ca2a5406ec2e0cd416b0
Jodiramadhan <jodiramadan167@gmail.com>
2023-09-25 19:26:31 +0700

    menambahkan latihan1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Jodi Ramadhan Saya tinggal di daerah Cibarusah Hobby yang saya sukai adalah Mendengarkan Musik";
+    }
+}
+?>
---
+++ application/controllers/latihan1.php
@@ -0,0 +1,16 @@
+<?php 
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Biodata Mahasiswa BSI</h1>";
+        echo "Nama : Jodi Ramadhan<br>";
+        echo "Nim : 19220018<br>";
+        echo "Kelas : 19.3A.24<br>";
+        echo "Umur : 21 tahun<br>";
+        echo "Alamat : Cibarusah<br>";
+        echo "No.HP : 08988671841<br>";
+        echo "Hobby : Mendengarkan Musik<br>";
+    }
+}
+?>
